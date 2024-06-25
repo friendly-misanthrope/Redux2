a7885ef93083ec6a690f1bb1c3ee6b54bb119b90
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-25 06:19:41 -0700

    hardcoded in initial state

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initalSlice = [
+  {id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things."},
+  {id: '2', 'title': 'Slices...', content: "The more I say slice, the more I want pizza"}
+]
+
