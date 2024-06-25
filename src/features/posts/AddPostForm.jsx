b460de86147fa36a6df0fd4a1ffa5c087ffaeccc
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-25 07:07:04 -0700

    created controlled form inputs for post title and content

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -15,6 +15,25 @@ const AddPostForm = () => {
   return (
     <section>
       <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="title">Post Title:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={handleTitleChange}
+        />
+
+        <label htmlFor="content">Post Content:</label>
+        <input
+          type="text"
+          id="content"
+          name="content"
+          value={content}
+          onChange={handleContentChange}
+        />
+      </form>
     </section>
   )
 }
